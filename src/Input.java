9a30b18429776d3133d358844aa28acbc42e920c
MarianaCesar <mari_carvalhomello@hotmail.com>
2017-02-06 23:20:19 -0200

    Tirando warnings da classe -ignora

--- src/Input.java
+++ src/Input.java
@@ -3,9 +3,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
